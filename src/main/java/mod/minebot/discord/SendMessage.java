7bf3e008187f365fd5980b8cb8359e0ff2a79049
SALTStation <stevenwilden2@web.de>
2019-06-03 19:23:48 +0200

    Normal Message

--- src/main/java/mod/minebot/discord/SendMessage.java
+++ src/main/java/mod/minebot/discord/SendMessage.java
@@ -2,7 +2,7 @@ package mod.minebot.discord;
 
 public class SendMessage {
     public SendMessage(String ChannelID,String Message){
-
+        ReferenceClass.guild.getTextChannelById(ChannelID).sendMessage(Message);
 
     }
 }
