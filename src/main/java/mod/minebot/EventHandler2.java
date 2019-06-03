3edd84c1b12704a0cc76a7999d0338ffaa451eae
GloriousAlpaca <31967704+GloriousAlpaca@users.noreply.github.com>
2019-06-03 21:30:57 +0200

    Event Hanlder Suggestion

--- src/main/java/mod/minebot/EventHandler2.java
+++ src/main/java/mod/minebot/EventHandler2.java
@@ -18,15 +18,19 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 
-@EventBusSubscriber
-public class EventHandler {
+public class EventHandler2 {
 	
+	
+	
+	@EventBusSubscriber
+	private class ConnectionEvent{
 	//Player Verbindungs Event
 	@SubscribeEvent
     public void playerConnecting(PlayerEvent.PlayerLoggedInEvent event){
 		String playername = event.player.getName();
 		String message = (playername+" is connecting to the server.");
 		//Discordbot sendet String
+	}
 	}
 	
 	//Player Verbindungstrennungs Event
